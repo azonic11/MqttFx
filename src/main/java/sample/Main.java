6955707c3992586cc3f4ec03655702fe943c0367
azonic11 <azonic11@gmail.com>
2018-05-19 19:25:10 +0300

    first local commit

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
 
-        primaryStage.setTitle("Hello World, first commit v2 (web editor)");
+        primaryStage.setTitle("Hello World, first commit v2 (web editor) + IDE edit");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
